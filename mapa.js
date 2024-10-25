d455150e9a3e1525b750f3bb070e6e72676768bb
guzmanrfj <guzmanrfj@gmail.com>
2024-10-24 21:10:25 -0600

    "Carga de Archivos"

---
+++ mapa.js
@@ -0,0 +1,5 @@
+let mapilla = L.map('mapa').setView([20.677522170309935, -103.34692535828518],6)
+
+L.tileLayer('https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png',{
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mapa);
