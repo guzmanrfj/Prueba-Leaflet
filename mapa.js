0f565f588bb200bb104a812f6b1260c95a2e8610
guzmanrfj <guzmanrfj@gmail.com>
2024-10-24 22:07:25 -0600

    "Se agrego un selector de ubicaciones"

--- mapa.js
+++ mapa.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([20.677522170309935, -103.34692535828518],12)
+let map = L.map('Mapa').setView([20.677522170309935, -103.34692535828518],12)
 
 L.tileLayer('https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png',{
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
